e0ed748086851ac4655dc48b0141848b400ba4dd
Joseph <josephylee72@gmail.com>
2023-11-15 20:16:35 -0800

    completed flask restful cr

--- server/app.py
+++ server/app.py
@@ -17,7 +17,38 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API"
+        }
+        return make_response(response_dict, 200)
+
+api.add_resource(Home, "/")
+
+class Newsletters(Resource):
+    def get(self):
+        responses = [item.to_dict() for item in Newsletter.query.all()]
+        return make_response(responses, 200)
+    
+    def post(self):
+        new_record = Newsletter(
+            title=request.get_json()["title"],
+            body=request.get_json()["body"],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        return make_response(new_record.to_dict(), 201)
+    
+api.add_resource(Newsletters, '/newsletters')
+
+class NewslettersById(Resource):
+    def get(self, id):
+        found = Newsletter.query.filter_by(id = id).first().to_dict()
+        return make_response(found, 200)
+
+api.add_resource(NewslettersById, '/newsletters/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
